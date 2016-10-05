3013831610eea2ea6342b702376e4b200518d0e2
drylib <author@drylib.com>
2016-10-05 13:45:35 -0400

    val.is.primitive

--- js/val.js
+++ js/val.js
@@ -2,7 +2,7 @@
 // You are NOT ALLOWED to modify and/or use this code without author permission
 "use strict";(function(){let val = drylib.val = {};
 
-val.comparer = (a,b)=>{
+val.compare = (a,b)=>{
     if (a === b || Number.isNaN(a) && Number.isNaN(b))
         return 0;
     if (a === undefined)
@@ -23,19 +23,34 @@ val.comparer = (a,b)=>{
         return 1;
 }
 
-{// use ===null instead of ==null because undefined==null
-    let assert = drylib.dbg.assert;
+let is = val.is = {};
 
-    assert(()=>1 && val.comparer(undefined,null) == -1)
-    assert(()=>2 && val.comparer(undefined,NaN) == -1)
-    assert(()=>3 && val.comparer(null,NaN) == -1)
-    assert(()=>4 && val.comparer(undefined,undefined) == 0)
-    assert(()=>5 && val.comparer(null,null) == 0)
-    assert(()=>6 && val.comparer(NaN,NaN) == 0)
-    assert(()=>7 && val.comparer(1,2) == -1)
-    assert(()=>8 && val.comparer('a','b') == -1)
-    assert(()=>9 && val.comparer('abc','ab') == 1)
-    assert(()=>10 && val.comparer('','') == 0)
-    assert(()=>11 && val.comparer('',NaN) == 1)
-}
-})()
+is.primitive = x => false
+        ||x === undefined
+        ||x === null
+        ||x instanceof Boolean
+        ||x instanceof Number
+        ||x instanceof String
+        ||x instanceof Symbol
+;
+
+{// use ===null instead of ==null because undefined==null
+    let dbg = drylib.dbg; let assert = dbg.assert; //dbg.assert.log = true;
+
+    assert(()=>1 && val.compare(undefined,null) == -1);
+    assert(()=>2 && val.compare(undefined,NaN) == -1);
+    assert(()=>3 && val.compare(null,NaN) == -1);
+    assert(()=>4 && val.compare(undefined,undefined) == 0);
+    assert(()=>5 && val.compare(null,null) == 0);
+    assert(()=>6 && val.compare(NaN,NaN) == 0);
+    assert(()=>7 && val.compare(1,2) == -1);
+    assert(()=>8 && val.compare('a','b') == -1);
+    assert(()=>9 && val.compare('abc','ab') == 1);
+    assert(()=>10 && val.compare('','') == 0);
+    assert(()=>11 && val.compare('',NaN) == 1);
+    assert(()=>12 && val.is.primitive(NaN));
+    assert(()=>13 && val.is.primitive(5));
+    assert(()=>14 && val.is.primitive(new Number(5)));
+    assert(()=>15 && val.is.primitive(new String('abc')));
+    assert(()=>16 && !val.is.primitive({}));
+}})();
