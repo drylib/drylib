7ec880bbcb2ad677b949e786351b4c8a6ea7e8f2
drylib <author@drylib.com>
2016-10-05 20:47:41 -0400

    val.is. primitive,sys

--- js/val.js
+++ js/val.js
@@ -28,19 +28,77 @@ let is = val.is = {};
 is.primitive = x => false
         ||x === undefined
         ||x === null
+        ||Number.isNaN(x)
         ||x instanceof Boolean
         ||x instanceof Number
         ||x instanceof String
         ||x instanceof Symbol
         ||x instanceof Date // have to include because (new Date()) instanceof Number == false
+        ||typeof x != 'object' && is.primitive(new Object(x))
 ;
 
 is.str = x => false
         ||x instanceof String
         ||typeof x == 'string'
 ;
-                         
-{// use ===null instead of ==null because undefined==null
+
+is.sys = x => false // true if x is system (non user-defined) object
+        // Mozilla list of Built-in objects
+        // with commented lines that cause error on Chrome
+        ||x instanceof Array
+        ||x instanceof ArrayBuffer
+        //||x instanceof Atomics
+        ||x instanceof Boolean
+        ||x instanceof DataView
+        ||x instanceof Date
+        ||x instanceof Error
+        ||x instanceof EvalError
+        ||x instanceof Float32Array
+        ||x instanceof Float64Array
+        ||x instanceof Function
+        //||x instanceof Generator
+        //||x instanceof GeneratorFunction
+        //||x instanceof Infinity
+        ||x instanceof Int16Array
+        ||x instanceof Int32Array
+        ||x instanceof Int8Array
+        //||x instanceof InternalError
+        //||x instanceof Intl
+        //||x instanceof Intl.Collator
+        //||x instanceof Intl.DateTimeFormat
+        //||x instanceof Intl.NumberFormat
+        //||x instanceof Iterator
+        //||x instanceof JSON
+        ||x instanceof Map
+        //||x instanceof Math
+        //||x instanceof NaN
+        ||x instanceof Number
+        //we want to have pure Object so we test for everything except Object ||x instanceof Object
+        //||x instanceof ParallelArray
+        ||x instanceof Promise
+        ||x instanceof Proxy
+        //||x instanceof RangeError
+        //||x instanceof ReferenceError
+        //||x instanceof Reflect
+        ||x instanceof RegExp
+        ||x instanceof Set
+        //||x instanceof SharedArrayBuffer
+        //||x instanceof StopIteration
+        ||x instanceof String
+        ||x instanceof Symbol
+        ||x instanceof SyntaxError
+        ||x instanceof TypeError
+        //||x instanceof TypedArray
+        ||x instanceof URIError
+        ||x instanceof Uint16Array
+        ||x instanceof Uint32Array
+        ||x instanceof Uint8Array
+        ||x instanceof Uint8ClampedArray
+        ||x instanceof WeakMap
+        ||x instanceof WeakSet
+;
+
+{// unit tests
     let dbg = drylib.dbg; let assert = dbg.assert; //dbg.assert.log = true;
 
     assert(()=>1 && val.compare(undefined,null) == -1);
@@ -61,4 +119,6 @@ is.str = x => false
     assert(()=>16 && !val.is.primitive({}));
     assert(()=>17 && !val.is.str({}));
     assert(()=>18 && val.is.str('abc') && val.is.str(new String('abc')));
+    assert(()=>19.1 && val.is.sys(new Map()));
+    assert(()=>19.2 && !val.is.sys({}));
 }})();
