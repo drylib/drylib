e6668ca21c6757d18eead49730ca082299d1b4c0
drylib <author@drylib.com>
2016-10-17 12:50:58 -0400

    indentation

--- js/sample.js
+++ js/sample.js
@@ -11,21 +11,22 @@
             return {
                 then: then,
                 resolve: res=>{
-                    for(let e of arr)
+                    for (let e of arr)
                         res = e(res);
                   return res;
                 },
             };
         };
-      return then(x=>x);
+        return then(x=>x);
     };
 
-    // ---- x.html ----
-
     var d = defer()
-      .then(function(res){ console.log('then-1', res); return 'ret-1'; })
-      .then(function(res){ console.log('then-2', res); return 'ret-2'; });
-      d.resolve('init');
+        .then(function(res){ console.log('then-1', res); return 'ret-1'; })
+        .then(function(res){ console.log('then-2', res); return 'ret-2'; })
+    ;
+    d.resolve('init');
       
     assert(()=>1 && d.resolve('init') == 'ret-2');
-}})();
+}
+
+})();
