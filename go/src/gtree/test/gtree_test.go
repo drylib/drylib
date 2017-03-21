4e97ad08ae9197c759cbf49a15425616b6e9ea75
drylib <author@drylib.com>
2017-03-20 22:04:34 -0400

    remove main from gtree_test

--- go/src/gtree/test/gtree_test.go
+++ go/src/gtree/test/gtree_test.go
@@ -2,7 +2,6 @@
 package tr_test
 
 import (
-	"fmt"
 	"testing"
 
 	"gtree"
@@ -85,37 +84,4 @@ func TestGet(t *testing.T) {
 			t.Fail()
 		}
 	})
-}
-
-func main() {
-	fmt.Println("started")
-	node := gtree.Node{}
-	node.Add("a")
-	if node.String() != "a" {
-		panic("node.String != 'a'")
-	}
-
-	node = gtree.Node{}
-	a := node.Get("a")
-	if a != node.Get("a") {
-		panic(`node.Get("a") != node.Get("a")`)
-	}
-
-	b := node.Get("b")
-	if a == b {
-		panic("a == b")
-	}
-	if node.String() != "" {
-		panic("node.String != ''")
-	}
-	node.Get("a").Get("c").Add("c1")
-	if node.Get("a").Get("c") != node.Get("a").Get("c") {
-		panic(`node.Get("a").Get("c") != node.Get("a").Get("c")`)
-	}
-	node.Get("a").Get("c").Add("c2")
-	if node.Get("a").Get("c").String() != "c1c2" {
-		panic("got " + node.Get("a").Get("c").String())
-	}
-
-	fmt.Println("finished")
 }
