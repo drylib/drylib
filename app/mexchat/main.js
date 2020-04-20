37bd913c946fd9cd94acf97bff1f45e3986c7049
drylib <author@drylib.com>
2020-04-20 16:47:38 +0300

    use main.less, buttons

--- app/mexchat/main.js
+++ app/mexchat/main.js
@@ -33,7 +33,8 @@ bot.run = ()=>{
     let head = tbl.thead().tr();
     head.th('time').text('Time');
     head.th('name').text('Name');
-    head.th('text').text('Text');
+    head.th('btn').text('Buttons');
+    head.th('text').text('Text')
     head.th('count').text('#');
     let u_ = {
       ui:tbl.tbody('user_'),
@@ -84,11 +85,25 @@ bot.run = ()=>{
         }
 
         for(let u of u_.map){
-          if(u.v){
-            //dbg.log(u.v)
-            if(u.v.ui) delete u.v.ui
             upd(u.v)
+        }
+
+        function pos(msg){
+          if(false && !stop){
+            stop = true;
+            mex.send(str.json({op: "unsubscribe",args: ["chat"]}));
           }
+          //dbg.log(msg)
+          let u = u_.map.get(msg.user) || {d:{k:msg.user, count:0}}
+          u.d.msg = msg.message
+          u.d.date = new Date(msg.date)
+          u.d.count++
+          save(u)
+          upd(u)
+        }
+        
+        function save(u){
+          u_.map.set(u.d.k, u)
         }
 
         function upd(u){
@@ -102,19 +117,70 @@ bot.run = ()=>{
 
           if(!ui){
             ui = u.ui = {}
-            if(msg.match(/position|XBTUSD|ETHUSD/gi) || d.fav || tr.fit(tl(d.k), tl('REKT')))
+            if(
+              (!d.ban || d.small)
+              && (msg.match(/position|XBTUSD|ETHUSD/gi) || d.fav || d.whale || tr.fit(tl(d.k), tl('REKT')) || d.k.startsWith('BitMEX'))
+            )
               ui.row = u_.ui.trPrepend()
             else
               ui.row = u_.ui.tr()
-            ui.time = ui.row.td('time').css("white-space", "nowrap")
-            ui.u = ui.row.td('name').btn().text(d.k)
-            function updu(){ui.u.css('color',d.fav?'green':'gray')}
-            updu()
-            ui.u.click(function(){
-              d.fav = !d.fav
-              u_.map.set(d.k, u)
-              updu()
-            })
+
+            let high = function(){
+              ui.row.detach()
+              u_.ui.prepend(ui.row)
+            }
+            let low = function(){
+              ui.row.detach()
+              u_.ui.append(ui.row)
+            }
+    
+            ui.time = ui.row.td('time')
+            ui.row.td('name').text(d.k)
+            {
+              let td = ui.row.td('btn')
+              {
+                let btn = td.btn().text('^')
+                let upd = function(){btn.clsOnoff('on',d.fav)}
+                upd()
+                btn.click(function(){
+                  if(d.fav = !d.fav) high();else low()
+                  save(u)
+                  upd()
+                })
+              }
+              {
+                let btn = td.btn().text('$')
+                let upd = function(){btn.clsOnoff('on',d.whale)}
+                upd()
+                btn.click(function(){
+                  if(d.whale = !d.whale) high();else low()
+                  save(u)
+                  upd()
+                })
+              }
+              {
+                let btn = td.btn().text('\\')
+                let upd = function(){btn.clsOnoff('on',d.ban)}
+                upd()
+                btn.click(function(){
+                  if(d.ban = !d.ban) low()
+                  save(u)
+                  upd()
+                })
+              }
+              {
+                let btn = td.btn().text('_')
+                let upd = function(){btn.clsOnoff('on',d.small)}
+                upd()
+                btn.click(function(){
+                  if(d.small = !d.small) low()
+                  save(u)
+                  upd()
+                })
+              }
+  
+            }
+
             ui.text = ui.row.td('text')
             ui.count = ui.row.td('count')
           }
@@ -123,20 +189,6 @@ bot.run = ()=>{
           ui.count.text(d.count)
         }
 
-
-        function pos(msg){
-          if(false && !stop){
-            stop = true;
-            mex.send(str.json({op: "unsubscribe",args: ["chat"]}));
-          }
-          //console.log(msg)
-          let u = u_.map.get(msg.user) || {d:{k:msg.user, count:0}}
-          u.d.msg = msg.message
-          u.d.date = new Date(msg.date)
-          u.d.count++
-          u_.map.set(u.d.k, u)
-          upd(u)
-        }
     }
 
     bot.init();
