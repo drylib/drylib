23e7e4ba2b4ab18437551979470b8eaf6ff696e4
drylib <author@drylib.com>
2020-04-23 00:05:37 +0300

    sections,filtering,size

--- app/mexchat/main.js
+++ app/mexchat/main.js
@@ -7,7 +7,7 @@ bot.run = ()=>{
     let limit = {msg: 500}
     let cfg = {dbg:false,reconn:true,}
     let cfgld = drylib.ld('cfg')
-    cfgld.dbg = true
+    cfgld.dbg = false
 
     let url="testnet"
     if(!test) url = "www"
@@ -66,28 +66,72 @@ bot.run = ()=>{
       })
     }
 
+    let tblWhalePos = body.tbl('chat whale pos');
+    {
+      let tbl = tblWhalePos
+      let head = tbl.thead().tr();
+      head.th('btn').text('Buttons');
+      head.th('role').text('Role');
+      head.th('time').text('Time');
+      head.th('name').text('Name');
+      head.th('count').text('#');
+      head.th('size').text('Size');
+      head.th('text').text('Text')
+      u_.uiWhalePos = tbl.tbody('user_');
+    }
+
+    let tblHigh = body.tbl('chat high');
+    {
+      let tbl = tblHigh
+      let head = tbl.thead().tr();
+      head.th('btn').text('Buttons');
+      head.th('role').text('Role');
+      head.th('time').text('Time');
+      head.th('name').text('Name');
+      head.th('count').text('#');
+      head.th('size').text('Size');
+      head.th('text').text('Text')
+      u_.uiHigh = tbl.tbody('user_');
+    }
+
     let tbl = body.tbl('chat');
-    let head = tbl.thead().tr();
-    head.th('time').text('Time');
-    head.th('name').text('Name');
-    head.th('btn').text('Buttons');
-    head.th('role').text('Role');
-    head.th('text').text('Text')
-    head.th('count').text('#');
+    {
+      let head = tbl.thead().tr();
+      head.th('btn').text('Buttons');
+      head.th('role').text('Role');
+      head.th('time').text('Time');
+      head.th('name').text('Name');
+      head.th('count').text('#');
+      head.th('size').text('Size');
+      head.th('text').text('Text')
+    }
     u_.ui = tbl.tbody('user_');
+
+    let tblLow = body.tbl('chat low');
+    {
+      let head = tblLow.thead().tr();
+      head.th('btn').text('Buttons');
+      head.th('role').text('Role');
+      head.th('time').text('Time');
+      head.th('name').text('Name');
+      head.th('count').text('#');
+      head.th('size').text('Size');
+      head.th('text').text('Text')
+    }
+    u_.uiLow = tblLow.tbody('user_');
 
     bot.init = ()=>{
         let connect = ()=>{
           mex = new WebSocket("wss://" + url + ".bitmex.com/realtime");
           mex.onopen = function (ev) {
-            console.log('Connected',ev);
+            console.log(dbg.logDate(),'Socket connected');
             subscribe.ui.prop('disabled', false);
             subscribe.ui.click();
           }
           mex.onclose = function(ev) {
-            console.log('Socket disconnected');
+            console.log(dbg.logDate(), 'Socket disconnected');
             if(cfg.reconn) setTimeout(function(){
-              console.log('Reconnecting');
+              console.log(dbg.logDate(),'Socket reconnecting');
               connect()
             }, 3000);
           };
@@ -110,17 +154,12 @@ bot.run = ()=>{
         function chat(chunk){
             for(let msg of chunk.data){
               if(msg.channelID == 1){
-                //if(msg.message.match(/^position/gi) || msg.message.length > 100)
-                  pos(msg)
+                  newItem(msg)
               }
             }
         }
 
-        for(let u of u_.map){
-            upd(u.v)
-        }
-
-        function pos(msg){
+        function newItem(msg){
           if(false && !stop){
             stop = true;
             mex.send(str.json({op: "unsubscribe",args: ["chat"]}));
@@ -133,11 +172,20 @@ bot.run = ()=>{
           d.admin = d.k.startsWith('BitMEX')
           d.count++
           let dmsg
-          if(msg.message.match(/^[/]position/gi)){
-            d.pos = {msg:dmsg=msg.message, date:d.date}
-          }else{
-            d.msg = dmsg = msg.html
+          if(msg.message.startsWith('/position')){
+            d.pos = {
+              msg:dmsg=msg.message.slice(17),
+              date:d.date
+            }
           }
+          else if(msg.message.startsWith('/orders')) d.ord = {
+            msg:dmsg=msg.message.slice(14),
+            date:d.date
+          }
+          else d.msg = dmsg = msg.html
+
+          //let size = 
+
           d.tv = dmsg.match(/www.tradingview.com/gi)
           d.yt = dmsg.match(/youtube.com/gi)
 
@@ -155,11 +203,28 @@ bot.run = ()=>{
           let ui = u.ui
 
           let msg = d.msg
-          if(d.pos) msg = d.pos.msg
+          let date = new Date(d.date)
+          if(d.pos) {
+            let pos = d.pos
+            msg = pos.msg
+            pos.msg = msg = msg.replace('\r','').replace('\n','')
+            date = new Date(pos.date)
+            let size = pos.msg.match(/([0-9,.]+)/gi)
+            if(size){
+              pos.size = size = size[size.length-2]
+              if(size){
+                size = Number(size.replace(',',''))
+                if(size > 30000)
+                  d.whale = true
+                if((pos.maxSize||0) < size) pos.maxSize = size
+              }
+            }
+          }
+          else if(d.ord) {msg = d.ord.msg; date = new Date(d.ord.date)}
+          
+
           msg = cfg.convertfn(msg)
           //msg = msg.substring(0,limit.msg)
-          let date = new Date(d.date)
-          if(d.pos) date = new Date(d.pos.date)
           //if(ui){ui.row.detach();ui=null}
 
           if(d.pos && msg.match(/<img/ig)) {if(ui)ui.row.detach();ui=null;return;}
@@ -168,25 +233,26 @@ bot.run = ()=>{
             ui = u.ui = {}
             ui.row = u_.ui.tr()
 
-            let high = function(on){
+            let place = function(on){
               ui.row.detach()
-              if(on) u_.ui.prepend(ui.row)
-              else u_.ui.append(ui.row)
+              if(d.whale && d.pos){
+                if(d.small) u_.uiWhalePos.append(ui.row)
+                else u_.uiWhalePos.prepend(ui.row)
+              }else{
+                if(d.fav||d.whale)
+                  if(d.small) u_.uiHigh.append(ui.row)
+                  else u_.uiHigh.prepend(ui.row)
+                else if(d.pos)
+                  if(d.small) u_.ui.append(ui.row)
+                  else u_.ui.prepend(ui.row)
+                else if((d.bot || d.admin || d.ord) && !d.small) u_.uiLow.prepend(ui.row)
+                else u_.uiLow.append(ui.row)
+              }
               return on
             }
 
-            let prepend = high(true
-              && !(d.ban || d.small)
-              && (false
-                ||d.pos
-                //|| msg.match(/position/gi)
-                || d.fav || d.whale || d.bot || d.admin
-                //|| tr.fit(tl(d.k), tl('REKT'))
-              )
-            );
+            place()
 
-            ui.time = ui.row.td('time')
-            ui.row.td('name').text(d.k)
             {
               let td = ui.row.td('btn')
               {
@@ -194,7 +260,8 @@ bot.run = ()=>{
                 let upd = function(){btn.clsOnoff(d.fav)}
                 upd()
                 btn.click(function(){
-                  high(d.fav = !d.fav)
+                  d.fav = !d.fav
+                  place()
                   save(u)
                   upd()
                 })
@@ -204,27 +271,28 @@ bot.run = ()=>{
                 let upd = function(){btn.clsOnoff(d.whale)}
                 upd()
                 btn.click(function(){
-                  high(d.whale = !d.whale)
+                  d.whale = !d.whale
+                  place()
                   save(u)
                   upd()
                 })
+              }
+              {
+                td.btn().text('_').syncClick(function(){this.clsOnoff(d.small)},
+                  function(){
+                    d.small = !d.small
+                    save(u)
+                    place()
+                  }
+                )
               }
               {
                 let btn = td.btn().text('\\')
                 let upd = function(){btn.clsOnoff(d.ban)}
                 upd()
                 btn.click(function(){
-                  if(d.ban = !d.ban) high(false)
-                  save(u)
-                  upd()
-                })
-              }
-              {
-                let btn = td.btn().text('_')
-                let upd = function(){btn.clsOnoff(d.small)}
-                upd()
-                btn.click(function(){
-                  if(d.small = !d.small) high(false)
+                  d.ban = !d.ban
+                  iplace()
                   save(u)
                   upd()
                 })
@@ -246,16 +314,26 @@ bot.run = ()=>{
               if(d.pos)td.span('pos','P')
             }
 
-            ui.text = ui.row.td('text')
+            ui.time = ui.row.td('time')
+            ui.row.td('name').text(d.k)
             ui.count = ui.row.td('count')
+            ui.size = ui.row.td('size')
+            ui.text = ui.row.td('text')
           }
-          ui.time.text(date.toLocaleTimeString())
-          if(d.pos)ui.text.text(msg.replace(/^[/]position/gi,'').replace(/:bitmex:/gi,'')) //html(msg)
-          else ui.text.html(msg) //html(msg)
+          ui.time.text(dbg.logDate(date))
+          if(d.pos)ui.text.text(msg.replace(/:bitmex:/gi,''))
+          else if(d.ord) ui.text.html(msg)
+          else ui.text.html(msg)
           ui.count.text(d.count)
-        }
+          if(d.pos && d.pos.maxSize) ui.size.text(Number(d.pos.maxSize).toLocaleString())
+        } //upd
 
+        for(let u of u_.map){
+          upd(u.v)
+        }
+    
     }
+
 
     bot.init();
 };
